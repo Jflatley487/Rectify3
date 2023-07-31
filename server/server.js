683d56189f841d76a97f76936f4e49dc2031baa2
Joseph Flatley <Jflatley487@gmail.com>
2023-07-30 22:09:21 -0500

    env

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const { ApolloServer, InMemoryLRUCache } = require("apollo-server-express");
 const path = require("path");
