a6000c2bf32168339b5d48ce62b62a90e3fbf253
Joseph Flatley <Jflatley487@gmail.com>
2023-07-31 01:01:46 -0500

    auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,10 +4,10 @@ const secret = "mysecretssshhhhhhh";
 const expiration = "2h";
 // Export an object containing two functions: 'authMiddleware' and 'signToken'
 module.exports = {
-  authMiddleware: function ({ req }) {
-    let token = req.body.token || req.query.token || req.headers.authorization;
-    if (req.headers.authorization) {
-      token = token.split(" ").pop().trim();
+  authMiddleware: function ({ req, res, next }) {
+    let token = extractTokenFromRequest(req);
+    if (!token) {
+      return next();
     }
 
     console.log("Received token: ", token);
@@ -21,12 +21,22 @@ module.exports = {
       console.log("Decoded token: ", data); 
       req.user = data;
     } catch {
-      console.log("Invalid token");
+      console.log("Invalid token", error.message);
+      return res.status(401).json({ error: "invalid token!" });
     }
-    return req;
   },
   signToken: function ({ email, username, _id }) {
     const payload = { email, username, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
+function extractTokenFromRequest(req) {
+  let token = req.body.token || req.query.token;
+
+  if (!token && req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    token = req.headers.authorization.substring(7);
+  }
+
+  return token;
+}
